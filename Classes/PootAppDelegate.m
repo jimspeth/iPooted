1eb10aa93b7ffcb95f6f0cbe39dce2bc949e52c4
Jim Speth <jim@dwnld.me>
2015-10-26 09:55:19 -0400

    Updating project and cleanup.

--- Classes/PootAppDelegate.m
+++ Classes/PootAppDelegate.m
@@ -9,22 +9,10 @@
 #import "PootAppDelegate.h"
 #import "PootViewController.h"
 
-static __inline__ int SSRandomIntBetween(int a, int b)
-{
-    int range = b - a < 0 ? b - a - 1 : b - a + 1;
-    int value = (int)(range * ((float)random() / (float)LONG_MAX));
-    return value == range ? a : a + value;
-}
-
 @implementation PootAppDelegate
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application
 {
-    srandomdev();
-    
-    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleBlackOpaque];
-    [[UIApplication sharedApplication] setStatusBarHidden:NO animated:YES];
-    
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
 }
@@ -32,7 +20,7 @@ static __inline__ int SSRandomIntBetween(int a, int b)
 - (IBAction)playOne:(id)sender
 {
     static SystemSoundID soundID[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int i = SSRandomIntBetween(0, 8);
+    int i = arc4random() % 8;
     if (!soundID[i]) {
         AudioServicesCreateSystemSoundID((__bridge CFURLRef)[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"%d", i + 1] ofType:@"caff"]], &soundID[i]);
     }
--- main.m
+++ main.m
@@ -9,7 +9,6 @@
 #import <UIKit/UIKit.h>
 
 int main(int argc, char *argv[]) {
-    
     @autoreleasepool {
         int retVal = UIApplicationMain(argc, argv, nil, nil);
         return retVal;
